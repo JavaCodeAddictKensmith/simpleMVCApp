961778977336450242adad5340c3217b6c4b050b
Kenneth.Ajah <kenneth.ajah@stanbicibtc.com>
2024-02-28 10:38:15 +0100

    first commit

---
+++ Vidly/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Vidly.Data
+{
+	public class ApplicationDbContext
+	{
+		public ApplicationDbContext()
+		{
+		}
+	}
+}
+
---
+++ Vidly/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Vidly.Models
+{
+	public class Category
+	{
+		public Category()
+		{
+		}
+	}
+}
+
